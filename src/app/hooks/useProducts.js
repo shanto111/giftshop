db78780775c9dd6132ddc48b7476c61bababce30
Hasibul Hasan Shanto <mdhasibulhasan165@gmail.com>
2025-08-28 00:28:50 +0600

    h

--- src/app/hooks/useProducts.js
+++ src/app/hooks/useProducts.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
 const fetchProducts = async () => {
-  const res = await fetch("http://localhost:3000/api/admin/product");
+  const res = await fetch("/api/admin/product");
   if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
 };
--- src/app/hooks/useSingleProduct.js
+++ src/app/hooks/useSingleProduct.js
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 
 const fetchSingleProduct = async (id) => {
-  const res = await fetch(`http://localhost:3000/api/product/${id}`);
+  const res = await fetch(`/api/product/${id}`);
   if (!res.ok) throw new Error("Failed to fetch single product");
   return res.json();
 };
 
 export default function useSingleProduct(id) {
   return useQuery({
-    queryKey: ["single-product", id], // id দিয়ে cache আলাদা হবে
+    queryKey: ["single-product", id],
     queryFn: () => fetchSingleProduct(id),
-    enabled: !!id, // id থাকলেই API call করবে
+    enabled: !!id,
   });
 }
